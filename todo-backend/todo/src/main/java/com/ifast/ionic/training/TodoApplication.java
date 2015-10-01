391a9ac602850be25292dd58a16d6efc2e82e18c
sckoh <soochengkoh@gmail.com>
2015-10-01 23:11:53 +0800

    todo backend

---
+++ todo-backend/todo/src/main/java/com/ifast/ionic/training/TodoApplication.java
@@ -0,0 +1,12 @@
+package com.ifast.ionic.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApplication.class, args);
+    }
+}
